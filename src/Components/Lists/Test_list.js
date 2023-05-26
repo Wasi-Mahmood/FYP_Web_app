ec00b49d9e26083c2beac60abafe88dc4b625e6a
affan <affan631@gmail.com>
2023-05-26 17:32:28 +0500

    FIREBASE WORK In Progress

--- src/Components/Lists/Test_list.js
+++ src/Components/Lists/Test_list.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useContext } from "react";
 import { watchListStore } from "./Your_lists";
@@ -7,8 +7,11 @@ import { watchListStore } from "./Your_lists";
 
 const  Test_list =() =>
 {
+    const location = useLocation()
+    const val = Object.values(location.state)[0]
+    console.log("🚀 ~ file: Test_list.js:11 ~ location:",val)
     const watchListData =  useContext(watchListStore)
-    console.log("🚀 ~ file: Test_list.js:11 ~ watchListData:", watchListData)
+    // console.log("🚀 ~ file: Test_list.js:11 ~ watchListData:", watchListData)
     return(
         <div>
             This is Test List {watchListData}
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import  { initializeApp  } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getFirestore} from "firebase/firestore/lite"
 
 
 const firebaseConfig = {
@@ -17,5 +18,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth =  getAuth();
+const db = getFirestore(app)
 
-export {app,auth};
+
+
+
+export {auth,db};
